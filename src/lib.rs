d63ea04fcecca4b3a060b5fc8752a6952a4bfa85
UnsuccessfulLaminator <>
2024-12-30 22:58:22 +0000

    Now accounts for ties in y

--- src/lib.rs
+++ src/lib.rs
@@ -22,19 +22,26 @@ pub fn xicor<T: Ord + Copy>(x: &[T], y: &[T]) -> f64 {
 
     let idcs = argsort(&y_ord);
     let y_ascending = permute(&y_ord, &idcs);
-    let r_ascending = cumulative_count(&y_ascending);
+    let r_ascending = cumulative_lte(&y_ascending);
+    let l_ascending = cumulative_gte(&y_ascending);
     let mut rs = vec![0; x.len()];
+    let mut ls = vec![0; x.len()];
 
-    for (i, r) in idcs.into_iter().zip(r_ascending) {
+    for ((i, r), l) in idcs.into_iter().zip(r_ascending).zip(l_ascending) {
         rs[i] = r;
+        ls[i] = l;
     }
 
-    let n = x.len() as f64;
     let rsum = rs.windows(2)
         .map(|win| win[0].abs_diff(win[1]))
         .sum::<usize>();
 
-    1.-3.*(rsum as f64)/(n*n-1.)
+    let n = x.len();
+    let lsum = ls.into_iter()
+        .map(|l| l*(n-l))
+        .sum::<usize>();
+
+    1.-(n as f64)*(rsum as f64)/(2.*lsum as f64)
 }
 
 // Return the indices that would sort the given array. That is, if you map the
@@ -55,18 +62,23 @@ fn permute<T: Copy>(arr: &[T], idcs: &[usize]) -> Vec<T> {
 
 // For every element in the array, count how many elements are less than or
 // equal to it. The array should be sorted before it is passed in.
-fn cumulative_count<T: PartialEq<T> + Copy>(arr: &[T]) -> Vec<usize> {
-    let mut counts: Vec<usize> = vec![0; arr.len()];
-    let mut prev = arr[arr.len()-1];
-    let mut prev_count = arr.len();
+fn cumulative_lte<T: PartialEq<T> + Copy>(arr: &[T]) -> Vec<usize> {
+    let mut counts: Vec<usize> = (1..=arr.len()).collect();
 
-    for (i, (c, &a)) in counts.iter_mut().zip(arr).enumerate().rev() {
-        if a == prev { *c = prev_count; }
-        else {
-            *c = i+1;
-            prev = a;
-            prev_count = *c;
-        }
+    for i in (0..arr.len()-1).rev() {
+        if arr[i] == arr[i+1] { counts[i] = counts[i+1]; }
+    }
+
+    counts
+}
+
+// For every element in the array, count how many elements are greater than or
+// equal to it. The array should be sorted before it is passed in.
+fn cumulative_gte<T: PartialEq<T> + Copy>(arr: &[T]) -> Vec<usize> {
+    let mut counts: Vec<usize> = (1..=arr.len()).rev().collect();
+
+    for i in 0..arr.len()-1 {
+        if arr[i+1] == arr[i] { counts[i+1] = counts[i]; }
     }
 
     counts
@@ -91,12 +103,11 @@ mod tests {
         let x: Vec<f32> = (0..1000).map(|i| i as f32/1000.).collect();
         let y: Vec<f32> = x.iter().map(|&x| (x*12.566).sin()).collect();
 
-        // There is strong correlation forwards, because y is a one-to-one
-        // function of x
-        assert_eq!(xicorf(&x, &y), 0.988032988032988);
+        // There is strong correlation forwards, because y is a function of x
+        assert_eq!(xicorf(&x, &y), 0.988033059619297);
 
         // There is only very weak correlation backwards, because x is not a
-        // one-to-one function of y
+        // function of y - this is a one-to-many relationship
         assert_eq!(xicorf(&y, &x), 0.06099306099306101);
     }
 
@@ -118,10 +129,18 @@ mod tests {
     }
     
     #[test]
-    fn test_cumulative_count() {
-        let arr = [1.1, 1.2, 2.5, 2.5, 2.5, 3., 8., 19., 19., 51.7, 100.];
-        let counts = [1, 2, 5, 5, 5, 6, 7, 9, 9, 10, 11];
+    fn test_cumulative_lte() {
+        let arr = [1.1, 1.1, 2.5, 2.5, 2.5, 3., 8., 19., 19., 51.7, 51.7];
+        let counts = [2, 2, 5, 5, 5, 6, 7, 9, 9, 11, 11];
 
-        assert_eq!(cumulative_count(&arr).as_slice(), &counts);
+        assert_eq!(cumulative_lte(&arr).as_slice(), &counts);
+    }
+    
+    #[test]
+    fn test_cumulative_gte() {
+        let arr = [1.1, 1.1, 2.5, 2.5, 2.5, 3., 8., 19., 19., 51.7, 100.];
+        let counts = [11, 11, 9, 9, 9, 6, 5, 4, 4, 2, 1];
+
+        assert_eq!(cumulative_gte(&arr).as_slice(), &counts);
     }
 }
